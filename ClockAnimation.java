bacfb852081dc4da1b7b315cae15d484acb93925
U10416027 <U10416027@go.utaipei.edu.tw>
2016-06-06 17:09:48 +0800

    Create ClockAnimation.java

---
+++ ClockAnimation.java
@@ -0,0 +1,29 @@
+//U10416027	董郡麟
+
+import java.util.Calendar; 
+import java.util.GregorianCalendar;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
+import javafx.geometry.Insets;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.util.Duration;
+import javafx.application.Application;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.event.ActionEvent;
+
+public class ClockAnimation extends Application{
+	
+	public void start(Stage primaryStage){
+	
+	}
+}
